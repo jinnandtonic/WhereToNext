274b1ba93fe947fc230990f5177a11472a830ca0
RyanMillett <rmillett@student.cccd.edu>
2017-10-26 20:21:46 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/wheretonext/CollegeListActivity.java
@@ -0,0 +1,111 @@
+package edu.orangecoastcollege.cs273.wheretonext;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class CollegeListActivity extends AppCompatActivity {
+
+    private DBHelper db;
+    private List<College> collegesList;
+    private CollegeListAdapter collegesListAdapter;
+    private ListView collegesListView;
+
+    private EditText mNameEditText;
+    private EditText mPopulationEditText;
+    private EditText mTuitionEditText;
+    private RatingBar mRatingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_college_list);
+
+        //this.deleteDatabase(DBHelper.DATABASE_NAME);
+        db = new DBHelper(this);
+
+        mNameEditText = (EditText) findViewById(R.id.nameEditText);
+        mPopulationEditText = (EditText) findViewById(R.id.populationEditText);
+        mTuitionEditText = (EditText) findViewById(R.id.tuitionEditText);
+        mRatingBar = (RatingBar) findViewById(R.id.collegeRatingBar);
+
+        // TODO: Comment this section out once the colleges below have been added to the database,
+        // TODO: so they are not added multiple times (prevent duplicate entries)
+        db.addCollege(new College("UC Berkeley", 42082, 14068, 4.7, "ucb.png"));
+        db.addCollege(new College("UC Irvine", 31551, 15026.47, 4.3, "uci.png"));
+        db.addCollege(new College("UC Los Angeles", 43301, 25308, 4.5, "ucla.png"));
+        db.addCollege(new College("UC San Diego", 33735, 20212, 4.4, "ucsd.png"));
+        db.addCollege(new College("CSU Fullerton", 38948, 6437, 4.5, "csuf.png"));
+        db.addCollege(new College("CSU Long Beach", 37430, 6452, 4.4, "csulb.png"));
+
+        // Fill the collegesList with all Colleges from the database
+        collegesList = db.getAllColleges();
+
+        // Connect the list adapter with the list
+        collegesListAdapter = new CollegeListAdapter(this, R.layout.college_list_item, collegesList);
+
+        // Set the list view to use the list adapter
+        collegesListView = (ListView) findViewById(R.id.collegeListView);
+        collegesListView.setAdapter(collegesListAdapter);
+    }
+
+    public void viewCollegeDetails(View view) {
+
+        // Implement the view college details using an Intent
+        Intent detailsIntent = new Intent(this, CollegeDetailsActivity.class);
+        LinearLayout selectedItem = (LinearLayout) view;
+        College selectedCollege = (College) selectedItem.getTag();
+
+        detailsIntent.putExtra("Name", selectedCollege.getName());
+        detailsIntent.putExtra("Population", selectedCollege.getPopulation());
+        detailsIntent.putExtra("Tuition", (float) selectedCollege.getTuition());
+        detailsIntent.putExtra("Rating", (float) selectedCollege.getRating());
+        detailsIntent.putExtra("ImageName", selectedCollege.getImageName());
+
+        startActivity(detailsIntent);
+    }
+
+    public void addCollege(View view) {
+
+        // code for when the user clicks on the addCollegeButton
+        String name = mNameEditText.getText().toString();
+        String population = mPopulationEditText.getText().toString();
+        String tuition = mTuitionEditText.getText().toString();
+        double rating = mRatingBar.getRating();
+
+        if (TextUtils.isEmpty(name) || TextUtils.isEmpty(population) || TextUtils.isEmpty(tuition))
+        {
+            Toast.makeText(this, "All information about college must be provided",
+                    Toast.LENGTH_SHORT).show();
+        } else
+        {
+            College newCollege = new College
+                    (
+                            name,
+                            Integer.parseInt(population),
+                            Double.parseDouble(tuition),
+                            rating
+                    );
+
+            // add college
+            db.addCollege(newCollege);
+            collegesList.add(newCollege);
+            collegesListAdapter.notifyDataSetChanged();
+
+            // reset fields
+            mNameEditText.setText("");
+            mPopulationEditText.setText("");
+            mTuitionEditText.setText("");
+            mRatingBar.setRating(0.0f);
+        }
+    }
+}
---
+++ app/src/main/java/edu/orangecoastcollege/cs273/wheretonext/DBHelper.java
@@ -0,0 +1,114 @@
+package edu.orangecoastcollege.cs273.wheretonext;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+class DBHelper extends SQLiteOpenHelper {
+
+    //TASK 1: DEFINE THE DATABASE VERSION, NAME AND TABLE NAME
+    static final String DATABASE_NAME = "WhereToNext";
+    private static final String DATABASE_TABLE = "Colleges";
+    private static final int DATABASE_VERSION = 1;
+
+
+    //TASK 2: DEFINE THE FIELDS (COLUMN NAMES) FOR THE TABLE
+    private static final String KEY_FIELD_ID = "id";
+    private static final String FIELD_NAME = "name";
+    private static final String FIELD_POPULATION = "population";
+    private static final String FIELD_TUITION = "tuition";
+    private static final String FIELD_RATING = "rating";
+    private static final String FIELD_IMAGE_NAME = "image_name";
+
+
+    public DBHelper(Context context){
+        super (context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate (SQLiteDatabase database){
+
+        // create the database
+        String table = "CREATE TABLE " + DATABASE_TABLE
+                + "("
+                + KEY_FIELD_ID + " INTEGER PRIMARY KEY,"
+                + FIELD_NAME + " TEXT,"
+                + FIELD_POPULATION + " INTEGER,"
+                + FIELD_TUITION + " REAL,"
+                + FIELD_RATING + " REAL,"
+                + FIELD_IMAGE_NAME + " TEXT"
+                + ")";
+        database.execSQL(table);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database,
+                          int oldVersion,
+                          int newVersion) {
+
+        // upgrade the database
+        database.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
+        onCreate(database);
+    }
+
+    //********** DATABASE OPERATIONS:  ADD, GETALL
+
+    public void addCollege(College college) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        // add a College to the database
+        values.put(FIELD_NAME, college.getName());
+        values.put(FIELD_POPULATION, college.getPopulation());
+        values.put(FIELD_TUITION, college.getTuition());
+        values.put(FIELD_RATING, college.getRating());
+        values.put(FIELD_IMAGE_NAME, college.getImageName());
+
+        long id = db.insert(DATABASE_TABLE, null, values);
+        college.setId(id);
+        db.close();
+    }
+
+    public ArrayList<College> getAllColleges() {
+        ArrayList<College> collegeList = new ArrayList<>();
+        SQLiteDatabase database = this.getReadableDatabase();
+
+        // get all the colleges from the database.
+        Cursor cursor = database.query(DATABASE_TABLE,
+                new String[]
+                        {
+                                KEY_FIELD_ID,
+                                FIELD_NAME,
+                                FIELD_POPULATION,
+                                FIELD_TUITION,
+                                FIELD_RATING,
+                                FIELD_IMAGE_NAME
+                        },
+                null, null, null, null, null);
+
+        if (cursor.moveToFirst())
+        {
+            do
+            {
+                College college = new College
+                        (
+                                cursor.getInt(0),
+                                cursor.getString(1),
+                                cursor.getInt(2),
+                                cursor.getDouble(3),
+                                cursor.getDouble(4),
+                                cursor.getString(5)
+                        );
+                collegeList.add(college);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        database.close();
+
+        return collegeList;
+    }
+}
